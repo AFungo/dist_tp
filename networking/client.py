5a938f20bb63a85f9677347cf3858bf3b527612b
AFungo <augustofungo@gmail.com>
2024-11-27 15:37:12 -0300

    Define server and client

---
+++ networking/client.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import logging
+
+import grpc
+import ticket_purchase_pb2
+import ticket_purchase_pb2_grpc
+
+
+def run():
+    print("Will try to get free seats ...")
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = ticket_purchase_pb2_grpc.TicketPurchaseStub(channel)
+        response = stub.GetFreeSeats(ticket_purchase_pb2.FreeSeatRequest)
+    print("Free seats: " + response.message)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
---
+++ networking/company_server.py
@@ -0,0 +1,26 @@
+from concurrent import futures
+import logging
+
+import grpc
+import ticket_purchase_pb2
+import ticket_purchase_pb2_grpc
+
+
+class TicketPurchaser(ticket_purchase_pb2_grpc.TicketPurchaseServicer):
+    def FreeSeatRequest(self, request, context):
+        return ticket_purchase_pb2.FreeSeatReply(message=" Hola Valen ")
+
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ticket_purchase_pb2_grpc.add_TicketPurchaseServicer_to_server(TicketPurchaser(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
